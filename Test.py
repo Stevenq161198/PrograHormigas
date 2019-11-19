f64ed1d31d19994946f33bc882d5b3e546698548
Emmanuel Castro <emacf23@gmail.com>
2019-11-19 14:59:09 -0600

    Push Men

--- Test.py
+++ Test.py
@@ -25,6 +25,7 @@ def oneByOne(timeLapse, trees, start_time):
         print("Hojas Totales: ", totalLeaves)
         return cantAnts
 
+
 #################################################################################
 
 def getCombinations(lenTrees):
@@ -46,39 +47,69 @@ def evaluateCombination(combination, maxTime):
     return combination
 
 
-def probabilistic(trees, maxTime):
-    combinations = getCombinations(len(trees))
-    bestCombination = combinations[random.randint(0, len(combinations) - 1)]
-    evaluateCombination(bestCombination, maxTime)
-    for prueba in range(0, 5):
-        ran = random.uniform(0.0, 1.0)
-        for _combination in combinations:
-            if _combination.probability > ran:
-                evaluateCombination(_combination, maxTime)
-                if _combination.quantLeaf >= bestCombination.quantLeaf and _combination.quantAnts <= bestCombination.quantAnts:
-                    bestCombination = _combination
-                    _combination.probability += 0.09
-                else:
-                    _combination.probability -= 0.5
-    return bestCombination
+def getBacks(trees):
+    allBacks = 0
+    for tree in trees:
+        allBacks += tree.total_distance
+    return allBacks
 
 
+def getProbability(trees, allBacks):
+    probabilities = []
+    for tree in trees:
+        probabilities.append((allBacks - tree.total_distance) / allBacks)
+    return probabilities
+
+
+def getTreeByProbability(random, probabilities):
+    treePosition = 0
+    for probability in probabilities:
+        if random <= 0:
+            break
+        treePosition += 1
+        random -= probability
+    return treePosition
+
+
+def probabilistic(quantAnts, probabilities, trees, time):
+    ants = []
+    for i in range(0, quantAnts):
+        r = random.uniform(0.0, 1.0)
+        position = getTreeByProbability(r, probabilities)
+        ants.append(position)
+    result = AntAdmin.evaluate(trees, ants, antSpeed, time)
+    return result
+
+
+def mainProbabilistic(trees, start_time, time):
+    quantAnts = oneByOne(500, trees, start_time)
+    allBacks = getBacks(trees)
+    probabilities = getProbability(trees, allBacks)
+    bestResult = probabilistic(quantAnts, probabilities, trees, time)
+    print("Probabilities",probabilities)
+    for test in range(0, 15):
+        result = probabilistic(quantAnts, probabilities, trees, time)
+        if result["leaf_count"] > bestResult["leaf_count"]:
+            bestResult = result
+    return bestResult
+
+
+trees = [
+    Tree("A", 3, 4),  # El nombre es opcional, es sólo para que se identifique en el HTML
+    Tree("B", 6, 8, 1, 1),  #  Ahora se puede enviar también el tamaño base y el porcentaje de crecimiento
+    Tree("C", 6, 6),
+    Tree("D", 10, 3),
+    Tree("E", 10, 3),
+    Tree("F", 11, 5),
+    Tree("G", 17, 7),
+    Tree("H", 17, 3),
+    Tree("I", 18, 2),
+    Tree("J", 20, 1),
+    Tree("K", 23, 18),
+    Tree("L", 25, 2)
+]
 if __name__ == "__main__":
     start_time = time()
-    trees = [
-        Tree("A", 3, 4),  # El nombre es opcional, es sólo para que se identifique en el HTML
-        Tree("B", 6, 8, 1, 1),  #  Ahora se puede enviar también el tamaño base y el porcentaje de crecimiento
-        Tree("C", 6, 6),
-        Tree("D", 10, 3),
-        Tree("E", 10, 3),
-        Tree("F", 11, 5),
-        Tree("G", 17, 7),
-        Tree("H", 17, 3),
-        Tree("I", 18, 2),
-        Tree("J", 20, 1),
-        Tree("K", 23, 18),
-        Tree("L", 25, 2)
-    ]
-    print("Best Combination:", probabilistic(trees, 500))
+    mainProbabilistic(trees, start_time, 500)
     elapsed_time = time() - start_time
     print("Elapsed time: %.10f seconds." % elapsed_time)
