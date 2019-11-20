95b6be440fc284c35302f2c300bb302fc210d4dd
Edgar <edgdav00@gmail.com>
2019-11-20 11:03:19 -0600

    gitflow-feature-stash: MousqueHerramientaMistertiosa

--- genetic.py
+++ genetic.py
@@ -43,4 +43,5 @@ def genetic(pTargetLength, pOptimalFit, pGeneSet, pAntsSpeed, pTime, pTrees, pSt
         if child.Aptitud >= pOptimalFit:
             return child
         bestDad = child
+    print('Mejor gen',bestDad)
     return bestDad
