29a3bdedf17b6a169d7ded6a4750820e88e3b83c
Emmanuel Castro <emacf23@gmail.com>
2019-11-19 23:50:54 -0600

    VIP Upgrade

--- Test.py
+++ Test.py
@@ -3,7 +3,6 @@ import random
 from time import time
 import multiprocessing
 
-from Combination import *
 from genetic import genetic
 from misc.ant_admin import AntAdmin
 from misc.tree import Tree
@@ -11,40 +10,36 @@ from misc.tree import Tree
 import json
 import string
 
-
 antSpeed = 1
 
 
-def oneByOne(timeLapse, trees, start_time):
+def oneByOne(pTrees):
     global antSpeed
+
+    cantAnts = 0
     totalLeaves = 0
-    while time() - start_time < timeLapse:
-        cantAnts = 0
-        totalLeaves = 0
-        for tree in trees:
-            timeElapsed = 2 * ((tree.x / antSpeed) + (tree.height / antSpeed))
-            cantAnts = math.floor(math.floor(timeElapsed) * (1 / antSpeed))
-            leavesXTree = math.floor(tree.leaves_count / cantAnts) * cantAnts
-            totalLeaves += leavesXTree
-        print("Cantidad Hormigas: ", cantAnts)
-        print("Hojas Totales: ", totalLeaves)
-        return cantAnts,totalLeaves
+    for tree in pTrees:
+        timeElapsed = 2 * ((tree.x / antSpeed) + (tree.height / antSpeed))
+        cantAnts = math.floor(math.floor(timeElapsed) * (1 / antSpeed))
+        leavesXTree = math.floor(tree.leaves_count / cantAnts) * cantAnts
+        totalLeaves += leavesXTree
+    print("Cantidad Hormigas: ", cantAnts)
+    print("Hojas Totales: ", totalLeaves)
+    return cantAnts, totalLeaves
 
 
-#################################################################################
-
-def getBacks(trees):
+def getBacks(pTrees):
     allBacks = 0
-    for tree in trees:
+    for tree in pTrees:
         allBacks += tree.total_distance
     return allBacks
 
 
-def getProbability(trees, allBacks):
+def getProbability(pTrees, pAllBacks):
     probabilities = []
     totalProb = 0
-    for tree in trees:
-        prob = 1 - tree.total_distance / allBacks
+    for tree in pTrees:
+        prob = 1 - tree.total_distance / pAllBacks
         probabilities.append(prob)
         totalProb += prob
     for index in range(0, len(probabilities)):
@@ -52,37 +47,38 @@ def getProbability(trees, allBacks):
     return probabilities
 
 
-def getTreeByProbability(random, probabilities):
+def getTreeByProbability(pRandom, pProbabilities):
     treePosition = 0
-    for probability in probabilities:
-        if random <= 0 or treePosition >= len(probabilities) - 1:
+    for probability in pProbabilities:
+        if pRandom <= 0 or treePosition >= len(pProbabilities) - 1:
             break
         treePosition += 1
-        random -= probability
+        pRandom -= probability
     return treePosition
 
 
-def probabilistic(quantAnts, probabilities, trees, time):
+def probabilistic(pQuantAnts, pProbabilities, pTrees, pTime):
     ants = []
-    for i in range(0, quantAnts):
+    for i in range(0, pQuantAnts):
         r = random.uniform(0.0, 1.0)
-        position = getTreeByProbability(r, probabilities)
+        position = getTreeByProbability(r, pProbabilities)
         ants.append(position)
-    result = AntAdmin.evaluate(trees, ants, antSpeed, time)
+    result = AntAdmin.evaluate(pTrees, ants, antSpeed, pTime)
     return result
 
 
-def mainProbabilistic(trees,quantAnts, start_time, time):
-    allBacks = getBacks(trees)
+def mainProbabilistic(pTrees, pQuantAnts, pStartTime, pTime, pTime_lapse):
+    allBacks = getBacks(pTrees)
     probabilities = getProbability(trees, allBacks)
-    bestResult = probabilistic(quantAnts, probabilities, trees, time)
+    bestResult = probabilistic(pQuantAnts, probabilities, pTrees, pTime)
 
-    for test in range(0, 15):
-        result = probabilistic(quantAnts, probabilities, trees, time)
+    while time() - pStartTime < pTime_lapse:
+        result = probabilistic(pQuantAnts, probabilities, pTrees, pTime)
         if result["leaf_count"] > bestResult["leaf_count"]:
             bestResult = result
     print(bestResult)
     return bestResult
+
 
 trees = []
 listaLetras = list(string.ascii_lowercase)
@@ -90,17 +86,15 @@ with open('test3.json') as json_file:
     data = json.load(json_file)
     indexLetras = 0
     for p in data['test']:
-        #print('PosX: ' + str(p['posX']))
-        #print('Length: ' + str(p['length']))
-        #print('Levels: ' + str(p['levels']))
-        #print('LeafLength: ' + str(p['leafLength']))
-        #print('')
-        tree = Tree("A",p['posX'],p['levels'],p['length'])
-        #tree = Tree("A", p['posX'], p['levels'])
+        # print('PosX: ' + str(p['posX']))
+        # print('Length: ' + str(p['length']))
+        # print('Levels: ' + str(p['levels']))
+        # print('LeafLength: ' + str(p['leafLength']))
+        # print('')
+        # tree = Tree("A",p['posX'],p['levels'],p['length'])
+        tree = Tree("A", p['posX'], p['levels'])
         trees.append(tree)
         indexLetras += 1
-
-
 
 """trees = [
     Tree("A", 3, 4),  # El nombre es opcional, es sólo para que se identifique en el HTML
@@ -115,32 +109,34 @@ with open('test3.json') as json_file:
     Tree("J", 20, 1),
     Tree("K", 23, 18),
     Tree("L", 25, 2)
-<<<<<<< HEAD
-
-]
+"""
 
 
-def generateGeneSet(trees):
+def generateGeneSet(pTrees):
     positions = []
     position = 1
-    while position < len(trees):
+    while position < len(pTrees):
         positions.append(position)
         position += 1
     return positions
-=======
-]"""
->>>>>>> develop
 
 
 if __name__ == "__main__":
     geneSet = generateGeneSet(trees)
+    tiempo = int(input("Digite el tiempo por favor: "))
+    quantAntsOO = oneByOne(trees)
     start_time = time()
-<<<<<<< HEAD
-    quantAnts = oneByOne(500, trees, start_time)
-    mainProbabilistic(trees,quantAnts[0], start_time, 500000)
-    print("Mejor",genetic(quantAnts[0], quantAnts[1], geneSet, antSpeed, 500000, trees))
-=======
-    mainProbabilistic(trees, start_time, 100000)
->>>>>>> develop
+    """
+    mainProbabilistic(trees, quantAntsOO[0], start_time, 900000, (tiempo * 0.2))
+    print("Mejor",genetic(quantAntsOO[0], quantAntsOO[1], geneSet, antSpeed, 900000, trees, start_time, (tiempo * 0.2)))
+    """
+    mainProbabilistic = multiprocessing.Process(target=mainProbabilistic, args=(trees, quantAntsOO[0], start_time, 900000, (tiempo * 0.2)))
+    genetic = multiprocessing.Process(target=genetic, args=(quantAntsOO[0], quantAntsOO[1], geneSet, antSpeed, 900000, trees, start_time, (tiempo * 0.2)))
+    mainProbabilistic.start()
+    genetic.start()
+
+    mainProbabilistic.join()
+    genetic.join()
     elapsed_time = time() - start_time
     print("Elapsed time: %.10f seconds." % elapsed_time)
+
--- genetic.py
+++ genetic.py
@@ -2,44 +2,45 @@ import random
 
 from Chromosomes import *
 from misc.ant_admin import AntAdmin
+from time import time
 
 
-def _generar_padre(longitud, geneSet, antsSpeed, time, trees):
+def _generar_padre(pLength, pGeneSet, pAntsSpeed, pTime, pTtrees):
     genes = []
-    while len(genes) < longitud:
-        genes.append(geneSet[random.randint(0, len(geneSet) - 1)])
-    result = AntAdmin.evaluate(trees, genes, antsSpeed, time)
-    return Chromosome(genes, result["leaf_count"],result["ant_count"], result["loop"])
+    while len(genes) < pLength:
+        genes.append(pGeneSet[random.randint(0, len(pGeneSet) - 1)])
+    result = AntAdmin.evaluate(pTtrees, genes, pAntsSpeed, pTime)
+    return Chromosome(genes, result["leaf_count"], result["ant_count"], result["loop"])
 
 
-def _mutar(padre, geneSet, time, trees):
-    index = random.randrange(0, len(padre.Genes))
-    geneChild = padre.Genes
+def _mutar(pFather, pGeneSet, pTime, pTrees):
+    index = random.randrange(0, len(pFather.Genes))
+    geneChild = pFather.Genes
     for quantChanges in range(0, 3):
-        nuevoGen, alterno, newGene = random.sample(geneSet, 3)
-        if nuevoGen == geneChild[index]:
+        newG, alterno, newGene = random.sample(pGeneSet, 3)
+        if newG == geneChild[index]:
             geneChild[index] = alterno
         else:
-            geneChild[index] = nuevoGen
-        #geneChild.append(newGene)
+            geneChild[index] = newG
+        # geneChild.append(newGene)
     genes = geneChild
     ##Cambiar Velocidad
-    result = AntAdmin.evaluate(trees, genes, 1, time)
-    return Chromosome(genes, result["leaf_count"],result["ant_count"], result["loop"])
+    result = AntAdmin.evaluate(pTrees, genes, 1, pTime)
+    return Chromosome(genes, result["leaf_count"], result["ant_count"], result["loop"])
 
 
-def genetic(longitudObjetivo, aptitudOptima, geneSet, antsSpeed, time, trees):
+def genetic(pTargetLength, pOptimalFit, pGeneSet, pAntsSpeed, pTime, pTrees, pStartTime, pTimeLapse):
     random.seed()
-    bestDad = _generar_padre(longitudObjetivo, geneSet, antsSpeed, time, trees)
-    if bestDad.Aptitud >= aptitudOptima:
+    bestDad = _generar_padre(pTargetLength, pGeneSet, pAntsSpeed, pTime, pTrees)
+    if bestDad.Aptitud >= pOptimalFit:
         return bestDad
-    for i in range(0, 200):
-        child = _mutar(bestDad, geneSet, time, trees)
+    while time() - pStartTime < pTimeLapse:
+        child = _mutar(bestDad, pGeneSet, pTime, pTrees)
         # print("Mejor Padre",mejorPadre)
         # print("Chico",child)
         if bestDad.Aptitud >= child.Aptitud:
             continue
-        if child.Aptitud >= aptitudOptima:
+        if child.Aptitud >= pOptimalFit:
             return child
         bestDad = child
     return bestDad
